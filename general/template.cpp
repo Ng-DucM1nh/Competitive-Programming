db055a56ce14a2d3c225a291a60d7c4d27e0b1ff
Ng-DucM1nh <89336448+Ng-DucM1nh@users.noreply.github.com>
2023-11-13 00:10:06 +0700

    Create template.cpp

---
+++ general/template.cpp
@@ -0,0 +1,30 @@
+// vegnim
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned int ui;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair <int,int> pii;
+typedef pair <ll,ll> pll;
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define task ""
+
+signed main()
+{
+    ios_base::sync_with_stdio(NULL); cin.tie(nullptr);
+    if (fopen("inp.txt","r")){
+        freopen("inp.txt","r",stdin);
+    }
+    if (fopen(task".inp","r")){
+        freopen(task".inp","r",stdin); freopen(task".out","w",stdout);
+    }
+
+    return 0;
+}
